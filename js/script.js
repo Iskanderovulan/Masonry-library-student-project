4a34eefc84d53b85ee5e5a13eb12d91c23c33ade
Ulan Iskanderov <iskanderovulan@gmail.com>
2022-02-06 00:10:43 +0600

    first commit

---
+++ js/script.js
@@ -0,0 +1,17 @@
+$('.grid').masonry({
+    itemSelector: '.grid-item',
+    columnWidth: '.grid-sizer',
+    percentPosition: true
+});
+
+
+// $('.mobile-btn').on('click', function () {
+//     $('.mobile-btn').toggleClass('show-mobile-btn');
+//     $('.header__nav').toggleClass('show-menu')
+// });
+//
+// $('.header__nav a').on('click', function () {
+//     $('.mobile-btn').removeClass('show-mobile-btn');
+//     $('.header__nav').removeClass('show-menu')
+// });
+
